059432b1c8ef40caa1d089c39d1ac4f6adc60a04
Alex Barron <alexbarron@gmail.com>
2015-07-24 12:33:12 -0700

    Finish tutorial

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+	before_action :authenticate_user!
+	
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = Comment.create(params[:comment].permit(:content))
+		@comment.user_id = current_user.id
+		@comment.post_id = @post.id
+
+		if @comment.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,63 @@
+class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:show, :index]
+	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+
+	def index
+		@posts = Post.all.order("created_at DESC")
+	end
+
+	def show
+		@comments = @post.comments.order("created_at DESC")
+		@random_post = Post.where.not(id: @post).order("RANDOM()").first
+	end
+
+	def new
+		@post = current_user.posts.build
+	end
+
+	def create
+		@post = current_user.posts.build(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	def upvote
+		@post.upvote_by current_user
+		redirect_to :back
+	end
+
+	def downvote
+		@post.downvote_by current_user
+		redirect_to :back
+	end
+
+	private
+
+		def find_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :description, :link, :image)
+		end
+end
